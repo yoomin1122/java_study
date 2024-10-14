6170c84d9d810c785c7b4894062fe4b28f0e123f
Yoo Ho <79196965+yoomin1122@users.noreply.github.com>
2024-10-15 00:50:46 +0900

    Create Hello.java

---
+++ 241013/Hello.java
@@ -0,0 +1,18 @@
+package chap01;
+
+// 클래스
+public class Hello {
+// 메인 메소드
+	public static void main(String[] args) {
+		// 메인 메소드 안에 데이터들과 메소드들을 작성
+		// Java언어에서는 문법에 맞춰 구문 작성이 끝난 후 세미콜론(;)을 통해 끝내줘야 함.
+		
+		// System.out.println()를 하게 되면 출력 후 줄넘김을 함.
+		System.out.println("Hello World!");
+		
+		// System.out.print()를 하게 되면 줄넘김 없이 출력 작동
+		System.out.print("Hello World!");
+
+	}
+
+}
